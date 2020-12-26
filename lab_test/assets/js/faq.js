f33e9eab5e922492ec1ab1ae090d1dfdf59415de
tanquangyds <68691733+tanquangyds@users.noreply.github.com>
2020-12-26 20:12:49 +0700

    final

--- lab_test/assets/js/faq.js
+++ lab_test/assets/js/faq.js
@@ -2,7 +2,9 @@ var faqTitle = document.querySelectorAll(".faq__item__title");
 var faqTitleArr = Array.from(faqTitle);
 faqTitleArr.forEach(function(item) {
     item.onclick = function() {
-        console.log(item.nextElementSibling);
+        item.classList.toggle('arrow-up');
+        item.classList.toggle('arrow-down');
         item.nextElementSibling.classList.toggle("display__block");
+        
     }
 })
--- lab_test/assets/js/logInFunc.js
+++ lab_test/assets/js/logInFunc.js
@@ -1,3 +1,8 @@
-var logInFunc = function (){
-    
-}
+var logInElement = document.getElementById('logInBtn');
+// var logInFunc = function() {
+//     window.location.href('./login.html');
+// }
+// // var logOutFunc = function() {
+// //     logInElement.innerHTML = "Login";
+// //     localStorage.removeItem("login");
+// //     logInElement.setAttribute("href", "")}
